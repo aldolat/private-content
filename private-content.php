d15b54c511d5b467e4bdae1a08ae5927764eb9bd
aldolat <aldolat@gmail.com>
2016-11-26 12:54:25 +0100

    Added compatibility with WP 4.7

--- private-content.php
+++ private-content.php
@@ -5,7 +5,7 @@
  * Plugin URI: http://dev.aldolat.it/projects/private-content/
  * Author: Aldo Latino
  * Author URI: http://www.aldolat.it/
- * Version: 4.2-dev
+ * Version: 4.1
  * License: GPLv3 or later
  * Text Domain: private
  * Domain Path: /languages/
